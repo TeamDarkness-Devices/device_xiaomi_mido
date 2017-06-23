dc811f091ad9ce9a1ad8f66b212cbac26acafd4f
AKabhishek <abhishekkaushik.star@gmail.com>
2017-06-23 11:18:17 +0000

    mido: Bring in DarkNess reDefined
    Change-Id: I5f1d39a66d632ebdc7aca025a00618267ebdfd49
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- dnd.mk
+++ dnd.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017 DarkNess reDefined Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,10 +17,10 @@
 
 $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common DarkNess reDefined stuff.
+$(call inherit-product, vendor/dnd/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_mido
+PRODUCT_NAME := dnd_mido
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
